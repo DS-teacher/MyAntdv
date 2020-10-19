57919009b05c3fd921e7d2fadc308f0d1de1651d
dongty1 <dongty1@yonyou.com>
2020-10-19 15:49:37 +0800

    20201019 增加穿梭框组件 主题取消居中

--- src/views/About.vue
+++ src/views/About.vue
@@ -267,6 +267,32 @@
             开关组件：
             <a-switch default-checked />
           </a-space>
+          <!-- 树选择组件 -->
+          <a-space>
+            <a-tree-select v-model="value" show-search style="width: 200px"
+              :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }" placeholder="Please select" allow-clear
+              tree-default-expand-all>
+              <a-tree-select-node key="0-1" value="parent 1" title="parent 1">
+                <a-tree-select-node key="0-1-1" value="parent 1-0" title="parent 1-0">
+                  <a-tree-select-node key="random" :selectable="false" value="leaf1" title="my leaf" />
+                  <a-tree-select-node key="random1" value="leaf2" title="your leaf" />
+                </a-tree-select-node>
+                <a-tree-select-node key="random2" value="parent 1-1" title="parent 1-1">
+                  <a-tree-select-node key="random3" value="sss">
+                    <b slot="title" style="color: #08c">sss</b>
+                  </a-tree-select-node>
+                </a-tree-select-node>
+              </a-tree-select-node>
+            </a-tree-select>
+          </a-space>
+          <!-- 穿梭框组件 -->
+          <a-sapce>
+            <a-transfer :data-source="mockData" :titles="['Source', 'Target']" :target-keys="targetKeys"
+              :selected-keys="selectedKeys" :render="item => item.title" :disabled="disabled" @change="handleChange"
+              @selectChange="handleSelectChange" @scroll="handleScroll" />
+            <a-switch un-checked-children="enabled" checked-children="disabled" :checked="disabled"
+              style="margin-top: 16px" @change="handleDisable" />
+          </a-sapce>
         </a-space>
       </div>
     </a-layout-content>
@@ -289,6 +315,17 @@
       IconFont
     },
     data() {
+      const mockData = [];
+      for (let i = 0; i < 20; i++) {
+        mockData.push({
+          key: i.toString(),
+          title: `content${i + 1}`,
+          description: `description of content${i + 1}`,
+          disabled: i % 3 < 1,
+        });
+      }
+
+      const oriTargetKeys = mockData.filter(item => +item.key % 3 > 1).map(item => item.key);
       return {
         size: "middle",
         options: [{
@@ -316,8 +353,35 @@
             }, ],
           },
         ],
+        treeExpandedKeys: [],
+        value: undefined,
+        mockData,
+        targetKeys: oriTargetKeys,
+        selectedKeys: ['1', '4'],
+        disabled: false,
       };
     },
-    methods: {}
+    methods: {
+      handleChange(nextTargetKeys, direction, moveKeys) {
+        this.targetKeys = nextTargetKeys;
+
+        console.log('targetKeys: ', nextTargetKeys);
+        console.log('direction: ', direction);
+        console.log('moveKeys: ', moveKeys);
+      },
+      handleSelectChange(sourceSelectedKeys, targetSelectedKeys) {
+        this.selectedKeys = [...sourceSelectedKeys, ...targetSelectedKeys];
+
+        console.log('sourceSelectedKeys: ', sourceSelectedKeys);
+        console.log('targetSelectedKeys: ', targetSelectedKeys);
+      },
+      handleScroll(direction, e) {
+        console.log('direction:', direction);
+        console.log('target:', e.target);
+      },
+      handleDisable(disabled) {
+        this.disabled = disabled;
+      },
+    }
   };
 </script>
